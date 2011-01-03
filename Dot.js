84b1e3415e3ca60ad99c414d89a81b872507453d
Russell Harmon <russ@eatnumber1.com>
2011-01-03 14:20:45 -0500

    Refactor into separate files. Start using jquery.

---
+++ Dot.js
@@ -0,0 +1,46 @@
+function whatsThePulsarsPeriod( lastPeriod ) {
+	var daysPerCycle = 29.5;
+	var hoursPerCycle = daysPerCycle * 24;
+	var minutesPerCycle = hoursPerCycle * 60;
+	var secondsPerCycle = minutesPerCycle * 60;
+	var millisPerCycle = secondsPerCycle * 1000;
+	return lastPeriod + millisPerCycle;
+}
+
+function whatsOurEtaMrSulu( time ) {
+	var now = wheresMyShipScotty(new Date()).getTime();
+	var nanosTill = time - now;
+	var secondsTill = nanosTill / 1000;
+	var minutesTill = secondsTill / 60;
+	var hoursTill = minutesTill / 60;
+	var daysTill = hoursTill / 24;
+	return daysTill;
+}
+
+function wheresMyShipScotty( d ) {
+	d.setHours(0);
+	d.setMinutes(0);
+	d.setSeconds(0);
+	d.setMilliseconds(0);
+	return d;
+}
+
+function engageMrSulu() {
+	var lastPeriod = localStorage["lastPeriod"];
+	// Captain, this is illogical. - Spock
+	var foo = whatsOurEtaMrSulu(whatsThePulsarsPeriod(parseInt(lastPeriod)));
+	$("#days").html(lastPeriod == null ? "?" : whatsOurEtaMrSulu(whatsThePulsarsPeriod(parseInt(lastPeriod))));
+}
+
+$("document").ready(function() {
+	engageMrSulu();
+
+	var tomorrow = new Date();
+	tomorrow.setDate(tomorrow.getDate() + 1);
+	setInterval(engageMrSulu, wheresMyShipScotty(tomorrow).getTime() - wheresMyShipScotty(new Date()).getTime());
+
+	$("#dot").click(function() {
+		localStorage["lastPeriod"] = wheresMyShipScotty(new Date()).getTime();
+		engageMrSulu();	
+	});
+});
--- Makefile
+++ Makefile
@@ -1,19 +1,32 @@
 INSTALL ?= install
-CP ?= cp
 MKDIR ?= mkdir
+LN ?= ln
 
-RESOURCES := Default.png Dot.html Icon.png Info.plist
+JQUERY_RESOURCES := jquery.js jquery.flip.js
+RESOURCES := Dot.html Dot.css Dot.js Default.png Icon.png Info.plist $(JQUERY_RESOURCES)
 WIDGET := Dot.wdgt
 
-.PHONY: all clean
+.PHONY: all clean depclean
 
 all: $(WIDGET)
 
+depclean: clean
+	$(MAKE) -C jquery clean
+
 clean:
-	$(RM) -r $(WIDGET)
+	$(RM) -r $(WIDGET) $(JQUERY_RESOURCES)
 
 $(WIDGET): $(RESOURCES)
 
+jquery/dist/jquery.js:
+	$(MAKE) -C jquery V=1 init jquery
+
+jquery.js: jquery/dist/jquery.js
+	$(LN) $^ $@
+
+jquery.flip.js:
+	$(LN) Flip-Jquery/$@ $@
+
 %.wdgt:
 	$(MKDIR) $@
-	$(CP) -r $^ $@ || $(RM) -r $@
+	$(LN) $^ $@ || $(RM) -r $@
